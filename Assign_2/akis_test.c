97ef5d06637bf973d5de9b100411058810215723
Akis90 <akisiochanas@gmail.com>
2024-10-27 19:39:48 +0200

    assign2 logger.so

---
+++ Assign_2/akis_test.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[]) {
+    char *filename = "test_file.txt";
+    char *message = "default text";
+
+    int opt;
+    while ((opt = getopt(argc, argv, "f:m:")) != -1) {
+        switch (opt) {
+            case 'f':
+                filename = optarg;
+                break;
+            case 'm':
+                message = optarg;
+                break;
+            default:
+            	exit(1);
+        }
+    }
+
+    printf("|||%s - %s|||\n", message, filename);
+
+    FILE *file = fopen(filename, "a");
+    if (file == NULL) {
+        perror("Error opening file");
+        exit(1);
+    }
+    fwrite(message, sizeof(char), strlen(message), file);
+    fwrite(message, sizeof(char), strlen(message), file);
+    
+    fclose(file);
+
+    return 0;
+}
