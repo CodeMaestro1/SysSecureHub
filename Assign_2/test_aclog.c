fed62cdfe479be592dd2a09ea03021be4f9f3e5f
CodeMaestro1 <constantinospis@gmail.com>
2024-10-28 17:16:46 +0200

    Small changes in the structure of code.

--- Assign_2/test_aclog.c
+++ Assign_2/test_aclog.c
@@ -7,7 +7,7 @@
 #define GENERAL_PERMISSIONS 0777          // Full permissions for owner, group, and others
 #define OWNER_GROUP_PERMISSIONS 0660      // Owner and group have read/write access
 #define OTHERS_PERMISSIONS 0007           // Only others have execute permissions
-#define BUFFER_SIZE 100
+#define BUFFER_MAX_SIZE 100
 
 // Set file permissions and handle errors
 void set_permissions(const char *filename, int permissions) {
@@ -16,7 +16,11 @@ void set_permissions(const char *filename, int permissions) {
     }
 }
 
-// Open and handle file operations with read or write modes
+/**
+ * A simply handler used to wrap file access operations.
+ * It opens a file with the given mode, reads or writes some data, and then closes the file.
+ * If the file cannot be opened, an error message is printed.
+ */
 void handle_file_access(const char *filename, const char *mode) {
     FILE *file = fopen(filename, mode);
     if (!file) {
@@ -24,7 +28,7 @@ void handle_file_access(const char *filename, const char *mode) {
         return;
     }
 
-    char buffer[BUFFER_SIZE] = "Test data";  // Test data to write
+    char buffer[BUFFER_MAX_SIZE] = "Test data";  // Test data to write
     if (strcmp(mode, "r") == 0) {
         memset(buffer, 0, sizeof(buffer));  // Clear buffer before reading
         fread(buffer, 1, sizeof(buffer) - 1, file);
@@ -35,7 +39,10 @@ void handle_file_access(const char *filename, const char *mode) {
     fclose(file);
 }
 
-// Simulate user access based on iteration index
+/**
+ * This function helps us to simulate user access to a file by setting the permissions
+ * and later accessing the file with read/or write mode.
+ */
 void simulate_user_access(const char *filename, int permission_type, int mode_index) {
     set_permissions(filename, permission_type);
 
@@ -47,13 +54,16 @@ void simulate_user_access(const char *filename, int permission_type, int mode_in
 }
 
 int main() {
-    const char *filenames[10] = { 
-        "file_0", "file_1", "file_2", "file_3", "file_4",
-        "file_5", "file_6", "file_7", "file_8", "file_9"
-    };
+    int i;
+	size_t bytes;
+	FILE *file;
+	char filenames[10][7] = {"file_0", "file_1", 
+			"file_2", "file_3", "file_4",
+			"file_5", "file_6", "file_7", 		
+			"file_8", "file_9"};
 
     // Create files and assign appropriate permissions
-    for (int i = 0; i < 10; i++) {
+    for (i = 0; i < 10; i++) {
         FILE *file = fopen(filenames[i], "w+");
         if (!file) {
             fprintf(stderr, "Failed to create %s: %s\n", filenames[i], strerror(errno));
@@ -63,8 +73,14 @@ int main() {
         fwrite(filenames[i], strlen(filenames[i]), 1, file);
         fclose(file);
 
-        int permissions = (i < 3) ? GENERAL_PERMISSIONS :
-                          (i <= 6) ? OWNER_GROUP_PERMISSIONS : OTHERS_PERMISSIONS;
+		int permissions;
+		if (i < 3) {
+			permissions = GENERAL_PERMISSIONS;
+		} else if (i <= 6) {
+			permissions = OWNER_GROUP_PERMISSIONS;
+		} else {
+			permissions = OTHERS_PERMISSIONS;
+		}
 
         simulate_user_access(filenames[i], permissions, i % 2);  // Alternate between read and write
     }
