a6b964ae04dd91549c9627db7c7713739c283994
CodeMaestro1 <constantinospis@gmail.com>
2025-01-05 21:01:29 +0200

    Corrected a few issues with the packet crafting and updated the sources.txt file

--- Assign_7/part2/main.py
+++ Assign_7/part2/main.py
@@ -1,4 +1,4 @@
-from scapy.all import IP, TCP, UDP, ICMP, wrpcap
+from scapy.all import IP, TCP, UDP, ICMP, wrpcap, Raw, DNS, DNSQR
 from base64 import b64encode 
 from datetime import datetime
 
@@ -8,41 +8,60 @@ STUDENT_ID = '2021030047-2021030008'
 def student_packet():
     timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     payload = f'{STUDENT_NAME} {timestamp}'
-    packet = IP(dst="192.168.1.1") / TCP(dport=54321) / payload
+    packet = IP(dst="192.168.1.1") / TCP(dport=54321) / Raw(load = payload) #Use RAW since we have a custom payload
 
     return [packet]
 
 def port_scan_packets():
     packets = list()
-    services = (80, 443, 22, 23, 21, 53, 554, 1433, 3389, 1883)
+    protocol_and_ports = {
+    "HTTP": 80,
+    "HTTPS": 443,
+    "SSH": 22,
+    "TELNET": 23,
+    "FTP": 21,
+    "DNS": 53,
+    "RTSP": 554,
+    "SQL": 1433,
+    "RDP": 3389,
+    "MQTT": 1883
+}
 
-    for port in services:
+    for service, port in protocol_and_ports.items():
         timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+        #Determine the protocol for each service
+        #Ideally we should use the flag 's'(SYN) for TCP protocol but for the sake of the assignment we will ignore it
+        protocol_packet = TCP(dport=port) if service != "DNS" else UDP(dport=port)
+
         payload = f'{STUDENT_NAME} {timestamp}'
-        packet = IP(dst="192.168.1.2") / TCP(dport=port) / payload
+        packet = IP(dst="192.168.1.2") / protocol_packet / Raw(load = payload)
         packets.append(packet)
 
     return packets
 
 def base64_malicious_packets():
     packets = list()
+    
+    #Encode the student ID in base64
+    encoded_payload = b64encode(STUDENT_ID.encode()).decode()
 
     for _ in range(5):
-        payload = b64encode(STUDENT_ID.encode()).decode()
-        packet = IP(dst="192.168.1.3") / TCP(dport=8080) / payload
+        
+        packet = IP(dst="192.168.1.3") / TCP(dport=8080) / Raw(load = encoded_payload)
         packets.append(packet)
 
     return packets
 
 def dns_suspricious_domain_packets():
-    payload = 'malicious.example.com'
-    packet = IP(dst="192.168.1.1") / UDP(dport=53) / payload
+    payload = "malicious.example.com"
+    packet = IP(dst="192.168.1.1") / UDP(dport=53) / DNS(rd=1,qd=DNSQR(qname=payload,qtype="A"))
 
     return [packet]
 
 def ping_test_packet():
     payload = 'PingTest-2025'
-    packet = IP(dst="192.168.1.4") / ICMP() / payload
+    packet = IP(dst="192.168.1.4") / ICMP() / Raw(load = payload)
 
     return [packet]
 
